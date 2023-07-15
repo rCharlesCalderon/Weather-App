7affd9d31435ca30f93756c1ab831a3156cd9647
Ruben Calderon <Rcaldeon0907@gmail.com>
2023-07-15 07:31:19 -0700

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { truncate } = require("lodash");
 
 
 async function getData(location) {
-  let url = `http://api.weatherapi.com/v1/current.json?key=154bc563fa33473cb2f81254231307&q=${location}`;
+  let url = `https://api.weatherapi.com/v1/current.json?key=154bc563fa33473cb2f81254231307&q=${location}`;
   fetch(url, {
     mode: "cors",
   })
@@ -13,9 +13,6 @@ async function getData(location) {
       console.log(response);
     });
 }
-
-getData("San Deigo"); //Invalid
-getData("Paris"); //valid
 
 let submitLocation = (() => {
   let locationValue = document.querySelector(".location");
