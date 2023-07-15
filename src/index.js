724ac6f7a47d70c5af98f91dea50c7db38a83993
Ruben Calderon <Rcaldeon0907@gmail.com>
2023-07-15 04:00:49 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const { truncate } = require("lodash");
 
+
 async function getData(location) {
   let url = `http://api.weatherapi.com/v1/current.json?key=154bc563fa33473cb2f81254231307&q=${location}`;
-  let text = document.querySelector(".text");
-  fetch(url)
+  fetch(url, {
+    mode: "cors",
+  })
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
       console.log(response);
-      //Write the functions that process the JSON data you’re
-      // getting from the API and return an object with only the data you require for your app.
     });
 }
 
 getData("Paris");
+
