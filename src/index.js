5786694c8032aae589a7790f76ccd9ea54f35368
Ruben Calderon <Rcaldeon0907@gmail.com>
2023-07-18 04:28:15 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const { truncate } = require("lodash");
-
+import "./style.css";
+import { locationData } from "./weatherData";
+import { temperatureData } from "./weatherData";
+import { weatherCondition } from "./weatherData";
+import { weatherFeeling } from "./weatherData";
+import { weatherHumidity } from "./weatherData";
+import { windSpeed } from "./weatherData";
+import { weatherGif } from "./weatherData";
 
 async function getData(location) {
   let url = `https://api.weatherapi.com/v1/current.json?key=154bc563fa33473cb2f81254231307&q=${location}`;
@@ -10,16 +17,30 @@ async function getData(location) {
       return response.json();
     })
     .then(function (response) {
-      console.log(response);
+      displayData(response);
+    })
+    .catch(function (err) {
+      console.log();
     });
 }
 
+function displayData(response) {
+  locationData(response);
+  temperatureData(response);
+  weatherGif(response);
+  weatherCondition(response);
+  weatherFeeling(response);
+  weatherHumidity(response);
+  windSpeed(response);
+}
+
 let submitLocation = (() => {
-  let locationValue = document.querySelector(".location");
-  let submitLocation = document.querySelector(".submit");
+  let locationValue = document.querySelector("#location");
+  let submitLocation = document.querySelector(".search-icon");
   submitLocation.addEventListener("click", (event) => {
     console.log(getData(locationValue.value));
     event.preventDefault();
   });
 })();
 
+ 
---
+++ src/weatherData.js
@@ -0,0 +1,31 @@
+export function locationData(response) {
+  console.log(response);
+  let location = document.querySelector(".location");
+  location.textContent = `${response.location.name}, ${response.location.country}`;
+}
+export function temperatureData(response) {
+  let temperature = document.querySelector(".temperature");
+  temperature.textContent = `${response.current.temp_f}°F`;
+}
+export function weatherCondition(response) {
+  let condition = document.querySelector(".condition");
+  condition.textContent = `${response.current.condition.text}`;
+}
+export function weatherFeeling(response) {
+  let feeling = document.querySelector(".feeling");
+  feeling.textContent = `${response.current.feelslike_f}°F`;
+}
+export function weatherHumidity(response) {
+  let humidity = document.querySelector(".humidity");
+  humidity.textContent = `${response.current.humidity}%`;
+}
+export function windSpeed(response) {
+  let speed = document.querySelector(".wind-speed");
+  speed.textContent = `${response.current.wind_mph} mph`;
+}
+
+export function weatherGif(response) {
+  let img = document.querySelector(".weather-gif");
+  img.src = response.current.condition.icon;
+  console.log(response.current.condition.icon);
+}
